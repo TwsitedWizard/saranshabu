7f4dfafe3c499afc23ea122b1ebb505beadfcc53
saranshabucct <saran.shabu.cct@gmail.com>
2025-07-21 18:45:44 +0530

    thirs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // 👈 ADD THIS IMPORT LINE
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
